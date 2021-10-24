97918cd55b86cc5a1cc12100ece34f7a64388df0
Jordan Ophoff <noreply>
2021-10-24 15:54:20 -0400

    fixed bugs removed CharCodes and default lowercase for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,42 @@
-
-// declarations
-let characterLength = "";
-let firmUppercase;
-let firmSymbols;
-let firmNumbers;
-
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+","[", "]", "{", "}", ";", ":", "'", '"', "<", ">", ",", ".", "|"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+ 
 function generatePassword() {
-  let characterLength = (prompt("Please enter number of characters."));
+  var characterLength = (prompt("Please enter number of characters."));
 // correct number loop
   while (characterLength <= 7 || characterLength >= 129) {
     alert("Please select between 8-128 characters!")
-    let characterLength = (prompt("Please enter number of characters."));
+    var characterLength = (prompt("Please enter number of characters."));
   break;
   }
-  let firmUppercase = confirm("Click OK for uppercase");
-  let firmSymbols = confirm("Click OK for symbols");
-  let firmNumbers = confirm("Click OK for numbers");
-
+  var firmLowercase = confirm("Click OK for lowercase")
+  var firmUppercase = confirm("Click OK for uppercase");
+  var firmSymbols = confirm("Click OK for symbols");
+  var firmNumbers = confirm("Click OK for numbers");
+  while (firmLowercase === false && firmUppercase === false && firmSymbols === false && firmNumbers === false) {
+    alert("Please select at least one option to proceed!") 
+    var firmLowercase = confirm("CLick OK for lowercase");
+    var firmUppercase = confirm("Click OK for uppercase");
+    var firmSymbols = confirm("Click OK for symbols");
+    var firmNumbers = confirm("Click OK for numbers");
+  }
   let userPassword = []
 
-  if (firmUppercase) {
+  if (firmLowercase) {
+    userPassword = userPassword.concat(lowercase)
+  }
+
+if (firmUppercase) {
     userPassword = userPassword.concat(uppercase)
   }
 
-  if (firmSymbols) {
+if (firmSymbols) {
     userPassword = userPassword.concat(symbols)
   }
 
-  if (firmNumbers) {
+ if (firmNumbers) {
     userPassword = userPassword.concat(numbers)
   }
 
@@ -35,7 +44,7 @@ function generatePassword() {
   let finalPassword = ""
 
   for (let i = 0; i < characterLength; i++) {
-    finalPassword = finalPassword + lowercase + userPassword[Math.floor(Math.random() * userPassword.length)];
+    finalPassword = finalPassword + userPassword[Math.floor(Math.random() * userPassword.length)];
     console.log(userPassword);
   }
 
@@ -58,10 +67,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-let lowercase = String.fromCharCode(97, 122)
-let uppercase = String.fromCharCode(65, 90)
-let symbols = String.fromCharCode(33, 47).concat
-  (String.fromCharCode(58, 64)).concat
-  (String.fromCharCode(91, 96)).concat
-  (String.fromCharCode(123, 126))
-let numbers = String.fromCharCode(48, 57)  
+
+
+
+// let lowercase = String.fromCharCode(97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122)
+// let uppercase = String.fromCharCode(65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90)
+// let symbols = String.fromCharCode(33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126)
+// let numbers = String.fromCharCode(48, 49, 50, 51, 52, 53, 54, 55, 56, 57)  
